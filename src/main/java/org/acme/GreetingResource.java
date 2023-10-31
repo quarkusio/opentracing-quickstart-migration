e4ff72d4bab4ea358efab2d6bb1685fef785e901
brunobat <brunobat@gmail.com>
2023-10-31 11:00:27 +0000

    4.a OpenTracing shim - before

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,43 @@
 package org.acme;
 
+import io.opentracing.Scope;
+import io.opentracing.Span;
+import io.opentracing.tag.Tags;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.opentracing.Traced;
 
 @Path("/hello")
+@ApplicationScoped
 public class GreetingResource {
+
+    @Inject
+    io.opentracing.Tracer legacyTracer;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Traced(operationName = "Not needed, will change the current span name")
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        // Add a tag to the active span
+        legacyTracer.activeSpan().setTag(Tags.COMPONENT, "GreetingResource");
+
+        // Create a manual inner span
+        Span innerSpan = legacyTracer.buildSpan("Count response chars").start();
+
+        try (Scope dbScope = legacyTracer.scopeManager().activate(innerSpan)) {
+            String response = "Hello from RESTEasy Reactive";
+            innerSpan.setTag("response-chars-count", response.length());
+            return response;
+        } catch (Exception e) {
+            innerSpan.setTag("error", true);
+            innerSpan.setTag("error.message", e.getMessage());
+            throw e;
+        } finally {
+            innerSpan.finish();
+        }
     }
 }
